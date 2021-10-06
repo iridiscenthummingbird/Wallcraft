47ae0b5763f3718be6a1a5f017ecd989e9caebca
iridiscent-hummingbird <0508dima@gmail.com>
2021-10-06 11:52:40 +0300

    Add class BackgroundImage

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Wallcraft',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
